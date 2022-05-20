ab493742b25d42099a3e1496447ab7e1d8994fa0
sunghwanKim <shkcreate@naver.com>
2022-05-20 16:16:33 +0900

    listPost 수정중

--- src/main/java/com/nhnacademy/jdbc/board/controller/web/PostPageController.java
+++ src/main/java/com/nhnacademy/jdbc/board/controller/web/PostPageController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,7 +65,10 @@ public class PostPageController {
     }
 
     @GetMapping(value = "/listPost")
-    public String infoPostForm() {
+    public String listPostForm(Model model) {
+        Optional<Post> post = postService.getPost();
+        model.addAttribute("post",post);
+
         return "index/listPost";
     }
 
--- src/main/java/com/nhnacademy/jdbc/board/post/mapper/PostMapper.java
+++ src/main/java/com/nhnacademy/jdbc/board/post/mapper/PostMapper.java
@@ -3,9 +3,10 @@ package com.nhnacademy.jdbc.board.post.mapper;
 import com.nhnacademy.jdbc.board.post.domain.Post;
 import java.util.List;
 import java.util.Optional;
+import org.apache.ibatis.annotations.Param;
 
 public interface PostMapper {
-    Optional<Post> selectPost(int BoardNo);
+    Optional<Post> selectPost(@Param("boardNo") int boardNo);
     List<Post> selectPosts();
     void registerPost(Post post);
     void updateContextByNo(String boardContext, String boardTitle, int boardNo);
